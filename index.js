ef833568fd8b672168f0374d68846db056b4ec4a
TimonKosgei <timonkosgei15209@gmail.com>
2024-10-22 12:25:02 +0300

    Did labs

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let multiply
+let num1 = 1
+let num2 = 62
+let num3 = 10
+let num4 = 6
+let mod = num3%num4
+let max = Math.max(4,20)
+let random = Math.floor(Math.random()*10)
+multiply = num1 * num2
